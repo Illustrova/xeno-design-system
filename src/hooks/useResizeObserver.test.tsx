19e66933ebc5aca3caa5791076f82dcf3bf17a60
Irina Illustrova <illustrova@gmail.com>
2022-11-02 13:06:22 +0200

    fix: get rid of warning about react 18 in tests

--- src/hooks/useResizeObserver.test.tsx
+++ src/hooks/useResizeObserver.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { MutableRefObject } from 'react';
 
 import { unobserve, observe, reset } from './MockResizeObserver';
