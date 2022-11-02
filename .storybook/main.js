4c427ab79e532a1505be435b786de3279706b720
Irina Illustrova <illustrova@gmail.com>
2022-11-02 22:39:31 +0200

    chore: add favicon in stb config

--- .storybook/main.js
+++ .storybook/main.js
@@ -35,4 +35,17 @@ module.exports = {
 
     return config;
   },
+  managerHead: (head, { configType }) => {
+    const baseUrl = process.env.BASE_URL ?? '';
+
+    head += `
+      <link rel="apple-touch-icon" sizes="180x180" href="${baseUrl}/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="${baseUrl}/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="${baseUrl}/favicon-16x16.png" />
+      <link rel="manifest" href="${baseUrl}/site.webmanifest" />
+      <link rel="mask-icon" href="${baseUrl}/safari-pinned-tab.svg" color="#81a5f2" />
+      <meta name="msapplication-TileColor" content="#baccf3" />
+      <meta name="theme-color" content="#ffffff" />`;
+    return head;
+  },
 };
